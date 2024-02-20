a029b2954ec557391e7d1d36d068278461b36f4b
fahim <fahimahamedweb@gmail.com>
2024-02-20 20:51:30 +0600

    variation added

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product,  ReviewRating, ProductGallery
+from .models import Product,  ReviewRating, ProductGallery,Variation
 import admin_thumbnails
 
 @admin_thumbnails.thumbnail('image')
@@ -14,7 +14,7 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 
-admin.site.register(Product, ProductAdmin)
-
+admin.site.register(Product, ProductAdmin,)
+admin.site.register(Variation)
 admin.site.register(ReviewRating)
 admin.site.register(ProductGallery)
