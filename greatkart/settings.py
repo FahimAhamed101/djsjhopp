5c8c95d2f414ad40d451636485b2f9e3d3f4e776
fahim <fahimahamedweb@gmail.com>
2025-01-11 00:48:34 +0600

    edited

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,10 +134,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-      "whitenoise.middleware.WhiteNoiseMiddleware", 
+    #  "whitenoise.middleware.WhiteNoiseMiddleware", 
      #'language.DefaultLanguageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    
+    "allauth.account.middleware.AccountMiddleware",
      
 ]
 
